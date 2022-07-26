2027c95c2cac85f710a07603c2a23300c2cab501
rajahindustani1995 <rajeshdambavale959@gmail.com>
2022-07-26 17:00:16 +0530

    [Rajesh] Added : Retrieve Data from the Table Employee_Payroll

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -12,10 +12,14 @@ Create Table Employee_Payroll (
 	StartDate datetime, 
 );
 
---UC3-insert into database
+--------UC3-Insert into Database------------------
 INSERT INTO employee_payroll(Name,Salary,startDate)
 VALUES ('Gitesh', 38000,'2010-05-08'),
 ('Ashish', 34000,'2004-02-05'),
 ('Vikram', 50000, '2020-07-09'),
 ('Pooja', 45000, '2021-04-05'),
 ('Archana', 49000, '2019-04-05');
+
+--------UC4-Retrieve all the Data from the Table---------
+select * from Employee_Payroll;
+select Id,Name from Employee_Payroll;
