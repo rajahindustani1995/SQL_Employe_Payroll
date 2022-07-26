bb152e382a9ea0366189d6ca93816ac46392a869
rajahindustani1995 <rajeshdambavale959@gmail.com>
2022-07-26 16:20:05 +0530

    [Rajesh] Added : Create a Table in Payroll_Service DataBase

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,5 +1,13 @@
 ---------Employee Payroll DataBase------------
 ---------UC1-Create Payroll Database----------
-create database Payroll_Service
+create database Payroll_Service;
 
-use Payroll_Service
+use Payroll_Service;
+
+----------UC2-Create a Table Employee_Payroll-------
+Create Table Employee_Payroll (
+	Id int identity(1,1) primary key,
+	Name varchar(200), 
+	Salary float,
+	StartDate datetime, 
+);
