c63ee45dc196e8bc71e61d31752936798a94f9de
rajahindustani1995 <rajeshdambavale959@gmail.com>
2022-07-26 16:52:53 +0530

    [Rajesh] Added : Insert Data into the Table Employee_Payroll

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -11,3 +11,11 @@ Create Table Employee_Payroll (
 	Salary float,
 	StartDate datetime, 
 );
+
+--UC3-insert into database
+INSERT INTO employee_payroll(Name,Salary,startDate)
+VALUES ('Gitesh', 38000,'2010-05-08'),
+('Ashish', 34000,'2004-02-05'),
+('Vikram', 50000, '2020-07-09'),
+('Pooja', 45000, '2021-04-05'),
+('Archana', 49000, '2019-04-05');
